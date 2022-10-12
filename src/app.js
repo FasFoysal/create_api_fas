84fc391d905b80e84c4882045ead3c9f06de6b6d
Fas Foysal <shounfoysal@gmail.com>
2022-10-12 11:39:31 +0600

    validate add/data err fix

--- src/app.js
+++ src/app.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const app = express();
-
+ 
 require('./monConn');
 const database = require("./monConn");
 app.use(express.json());
-
 const port = process.env.PORT ||80;
-
-app.get('/',(req,res)=>{
-  res.send("Hello")
-})
-app.post('/add', async(req,res)=>{
-  try{
-const finalData = await new database(req.body);
-res.status(201).send(finalData);
-console.log(finalData);
-finalData.save();
-  }catch(err){
-console.log('data not send')
-  }
+app.post('/add',async (req,res)=>{
+ try{
+   const finalData = new database(req.body);
+   await finalData.save()
+   res.status(201).send(finalData);
+   console.log(finalData);
+ }catch(err){
+   res.status(400).send('data not send something wrong')
+ }
+// const finalData = new database(req.body);
+// finalData.save().then(()=>{
+//   res.status(201).send(finalData);
+//   console.log(finalData);
+// }).catch((err)=>{
+//  res.status(400).send('data not send something wrong')
+// })
 })
 app.listen(port,()=>{
-  console.log(`port open in ${port}`)
+ console.log(`port open in ${port}`)
 })
--- src/monConn.js
+++ src/monConn.js
@@ -1,13 +1,33 @@
 const mongoose = require("mongoose");
+var validator = require('validator');
+
 main().then(()=>console.log('Connnet success mongo')).catch( (err)=> console.log('Disconnect to mongo'));
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/Fas'); 
 };
 const condition = new mongoose.Schema({
   name:String,
-  roll:Number,
-  class:Number,
-  gamil:String,
+  roll:{
+    type:Number,
+    unique:true,
+    minLingth:1,
+    maxLength:2
+  },
+  class:{
+    type:Number,
+    minLingth:1,
+    maxLength:2
+  },
+  gmail:{
+type:String,
+unique:true,
+validate(val){
+    if(!validator.isEmail(val)){
+      // Error "E" shout be capital latter
+      throw new Error("Email Not valid");
+    }
+}
+  }
 })
 const database = mongoose.model('studentsList', condition);
 module.exports = database;
