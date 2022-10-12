cc41183dcc63c676d2febe38230df90a4074f073
Fas Foysal <shounfoysal@gmail.com>
2022-10-12 12:38:56 +0600

    search by roll

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ require('./monConn');
 const database = require("./monConn");
 app.use(express.json());
 const port = process.env.PORT ||80;
-app.post('/add',async (req,res)=>{
+
+// post add data
+app.post('/student',async (req,res)=>{
  try{
    const finalData = new database(req.body);
    await finalData.save()
@@ -22,6 +24,28 @@ app.post('/add',async (req,res)=>{
 //  res.status(400).send('data not send something wrong')
 // })
 })
+
+// get show all datas
+app.get("/student",async(req,res)=>{
+try{
+   const studentData = await database.find();
+   res.send(studentData).status(200);
+}catch(err){
+  res.status(401).send("Some thing wrong"+err);
+}
+})
+// get singal data
+app.get("/student/:roll",async(req,res)=>{
+  try{
+    const _id = req.params.roll;
+     const studentSD = await database.find({roll:_id});
+     res.send(studentSD).status(200);
+  }catch(err){
+    res.status(401).send("Some thing wrong"+err);
+  }
+  })
+
+// port connections
 app.listen(port,()=>{
  console.log(`port open in ${port}`)
 })
