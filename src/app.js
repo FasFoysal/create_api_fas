4274b9868755b3031dbdfeb6c57e17ca206ba686
Fas Foysal <shounfoysal@gmail.com>
2022-10-29 14:02:09 +0600

    add render page

--- src/app.js
+++ src/app.js
@@ -1,36 +1,70 @@
+const { assert } = require("console");
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const hbs = require('hbs');
+const path = require("path");
+const { nextTick } = require("process");
 
 require('./monConn');
 const database = require("./monConn");
 app.use(express.json());
 const port = process.env.PORT ||80;
 
-// post add data
-app.post('/student',async (req,res)=>{
- try{
-   const finalData = new database(req.body);
-   await finalData.save()
-   res.status(201).send(finalData);
-   console.log(finalData);
- }catch(err){
-   res.status(400).send('data not send something wrong')
- }
-})
+const partialPath = path.join(__dirname,"../template/partials");
+const viewsPath = path.join(__dirname, '../template');
+app.use('/bootstrap', express.static(path.join(__dirname, '../node_modules/bootstrap/dist')))
+app.use('/style', express.static(path.join(__dirname, '../template/style')))
+app.set('views',viewsPath);
+app.set('view engine', 'hbs');
+hbs.registerPartials(partialPath);
+app.use(express.urlencoded());
+
+
+app.get("/",(req,res)=>{
+  res.render("index")
+ })
+app.get("/students", (req,res)=>{
+  res.render("students")
+ })
+
+ app.post("/students", async(req,res)=>{
+  try{
+    let getRoll =  req.body.getRoll;
+    console.log(getRoll)
+    const studentData = await database.find({roll:getRoll});
+    studentData.map(val=>{
+      res.status(200).render("students",{name:val.name,class:val.class,roll:val.roll,gmail:val.gmail,id:val._id})
+    }) 
+    res.status(200).render("students",{notMatch:"Roll not Match"})
+    console.log(studentData)
+  }catch(err){
+    res.status(200).render("students",{notMatch:"Roll not Match"})
+  }
+ })
 
 // get show all datas
-app.get("/student",async(req,res)=>{
-try{
-   const studentData = await database.find().sort({roll:1});
-    const convertJson =  JSON.stringify(studentData)
-   fs.writeFile("../data.json",convertJson,"utf-8",(err)=>{
-   })
-   res.send(studentData).status(200);
-}catch(err){
-  res.status(401).send("Some thing wrong"+err);
-}
-})
+ app.get("/student",async(req,res)=>{
+    try{
+      const studentData = await database.find().sort({roll:1});
+      res.send(studentData);
+    }catch(err){
+      res.status(401).send("Some thing wrong"+err);
+    }
+    })
+
+// post add data
+app.post('/student',async (req,res)=>{
+  try{
+    const finalData = new database(req.body);
+    await finalData.save()
+    res.status(201).send(finalData);
+    console.log(finalData);
+  }catch(err){
+    res.status(400).send('data not send something wrong')
+  }
+ })
+ 
 // get singal data
 app.get("/student/:roll",async(req,res)=>{
   try{
--- src/fas.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-
-fs.writeFileSync('../text.txt',"hi my name is Foysal ahmed shoun|");
-fs.appendFileSync('../text.txt','I am fine how are you|');
-fs.appendFileSync('../text.txt','I am not fine how are you|||||');
-fs.appendFileSync('../text.txt','|||||... in new one');
